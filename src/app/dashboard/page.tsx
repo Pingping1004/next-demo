1f7dfda4dc8511a8efbced66210420bdb8a2a391
Ping <piyatanakj@gmail.com>
2025-02-24 02:11:07 +0700

    Initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { auth, currentUser } from '@clerk/nextjs/server'
+
+export default async function DashboardPage() {
+    const authObj = await auth();
+    const userObj = await currentUser();
+
+    console.log(authObj, userObj);
+    
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  )
+}
